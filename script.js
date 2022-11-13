d144901fc866f501bf1dbf88da02a780f3cabd5a
alyssa <110448688+alyssapineda@users.noreply.github.com>
2022-11-13 21:32:47 +1300

    password length input slider

--- script.js
+++ script.js
@@ -64,5 +64,33 @@ function copyPasswordEl2() {
   cb.writeText(copiedPassword.innerText).then(() => alert('Password Option 2 copied'));
 }
 
+//SETTING PASSWORD LENGTH
+const sliderProps = {
+	fill: "#6D28D9",
+	background: "#fff",
+};
+// Selecting the Range Slider container which will effect the LENGTH property of the password.
+const slider = document.querySelector(".slider-container");
 
+// Text which will show the value of the range slider.
+const sliderValue = document.querySelector("#length__title");
 
+// Using Event Listener to apply the fill and also change the value of the text.
+slider.querySelector("input").addEventListener("input", event => {
+	sliderValue.setAttribute("data-length", event.target.value);
+	applyFill(event.target);
+});
+// Selecting the range input and passing it in the applyFill func.
+applyFill(slider.querySelector("input"));
+
+// This function is responsible to create the trailing color and setting the fill.
+function applyFill(slider) {
+	const percentage = (100 * (slider.value - slider.min)) / (slider.max - slider.min);
+	const bg = `linear-gradient(90deg, ${sliderProps.fill} ${percentage}%, ${sliderProps.background} ${percentage +
+			0.1}%)`;
+	slider.style.background = bg;
+	sliderValue.setAttribute("data-length", slider.value);
+}
+
+// The input slider, will use to change the length of the password
+const lengthEl = document.getElementById("slider");
