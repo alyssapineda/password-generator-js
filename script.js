063d7e05bc847de8551f053fb027eced8c4a9446
alyssa <110448688+alyssapineda@users.noreply.github.com>
2022-11-14 01:15:18 +1300

    password length slider fixed

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //FUTURE WORK:
-//set password length - user input?
 //Toggle "symbols" and "numbers" on/off
 //add password entropy percentage
 
@@ -13,6 +12,17 @@ let passwordEl2 = document.querySelector("#password-el2")
 let psContainer1 = []
 let psContainer2 = []
 
+let passwordLength = document.querySelector("#length-title")
+let range = document.querySelector('#slider')
+
+range.addEventListener('mouseup', function () {
+  if (this.value > 0 && this.value < 5) {
+    passwordLength.textContent = "Password Length: " + slider.value
+  }else {
+    passwordLength.textContent = "Password Length: " + slider.value
+  }
+})
+
 function generatePasswords() {
   clearPassword()
   generatePassword1()
@@ -21,7 +31,8 @@ function generatePasswords() {
 
 function generatePassword1() {
 
-  for (let i = 0;i < 15; i++) {
+
+  for (let i = 0;i < slider.value; i++) {
     let randomCharacter = Math.floor(Math.random() * characters.length)
     psContainer1.push(characters[randomCharacter])
   }
@@ -29,11 +40,13 @@ function generatePassword1() {
   for (let i = 0;i <psContainer1.length;i++) {
     passwordEl1.textContent += psContainer1[i]
   }
+  
 }
 
 function generatePassword2() {
 
-  for (let i = 0;i < 15; i++) {
+let range = document.getElementsByTagName('slider');
+  for (let i = 0;i < slider.value; i++) {
     let randomCharacter = Math.floor(Math.random() * characters.length)
     psContainer2.push(characters[randomCharacter])
   }
@@ -41,6 +54,7 @@ function generatePassword2() {
   for (let i = 0;i <psContainer2.length;i++) {
     passwordEl2.textContent += psContainer2[i]
   }
+  
 }
 
 function clearPassword() {
@@ -65,32 +79,29 @@ function copyPasswordEl2() {
 }
 
 //SETTING PASSWORD LENGTH
-const sliderProps = {
-	fill: "#6D28D9",
-	background: "#fff",
-};
-// Selecting the Range Slider container which will effect the LENGTH property of the password.
-const slider = document.querySelector(".slider-container");
+// const sliderProps = {
+// 	fill: "#6D28D9",
+// 	background: "#fff",
+// };
+// // Selecting the Range Slider container which will effect the LENGTH property of the password.
+// const slider = document.querySelector(".slider-container");
 
-// Text which will show the value of the range slider.
-const sliderValue = document.querySelector("#length__title");
+// // Text which will show the value of the range slider.
+// const sliderValue = document.querySelector("#length__title");
 
-// Using Event Listener to apply the fill and also change the value of the text.
-slider.querySelector("input").addEventListener("input", event => {
-	sliderValue.setAttribute("data-length", event.target.value);
-	applyFill(event.target);
-});
-// Selecting the range input and passing it in the applyFill func.
-applyFill(slider.querySelector("input"));
+// // Using Event Listener to apply the fill and also change the value of the text.
+// slider.querySelector("input").addEventListener("input", event => {
+// 	sliderValue.setAttribute("data-length", event.target.value);
+// 	applyFill(event.target);
+// });
+// // Selecting the range input and passing it in the applyFill func.
+// applyFill(slider.querySelector("input"));
 
-// This function is responsible to create the trailing color and setting the fill.
-function applyFill(slider) {
-	const percentage = (100 * (slider.value - slider.min)) / (slider.max - slider.min);
-	const bg = `linear-gradient(90deg, ${sliderProps.fill} ${percentage}%, ${sliderProps.background} ${percentage +
-			0.1}%)`;
-	slider.style.background = bg;
-	sliderValue.setAttribute("data-length", slider.value);
-}
-
-// The input slider, will use to change the length of the password
-const lengthEl = document.getElementById("slider");
+// // This function is responsible to create the trailing color and setting the fill.
+// function applyFill(slider) {
+// 	const percentage = (100 * (slider.value - slider.min)) / (slider.max - slider.min);
+// 	const bg = `linear-gradient(90deg, ${sliderProps.fill} ${percentage}%, ${sliderProps.background} ${percentage +
+// 			0.1}%)`;
+// 	slider.style.background = bg;
+// 	sliderValue.setAttribute("data-length", slider.value);
+// }
